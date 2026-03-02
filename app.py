7bc213163b943819edea6cec941bcc69a978bcb7
Vivek Agarwal <viveksinghalp@gmail.com>
2026-03-02 22:36:44 +0530

    cleanup

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_smorest import Api
-from api.bbmpcomplaint import complaint_bp
-from api.bbmp_complaint_report import complaint_report_bp
 from api.health import health_bp
 from api.web_scrap import scrape_bp
 from config import DEBUG
@@ -25,8 +23,6 @@ def create_app() -> Flask:
 
     # -- Smorest-managed blueprints (auto-documented) --------------------------
     smorest_api = Api(app)
-    smorest_api.register_blueprint(complaint_bp)
-    smorest_api.register_blueprint(complaint_report_bp)
     smorest_api.register_blueprint(health_bp)
     smorest_api.register_blueprint(scrape_bp)
 
